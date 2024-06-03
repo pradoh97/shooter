766ef2ce065e0fdd211ef11fb4fd01db2a186088
Hernán <pradoh97@gmail.com>
2024-06-02 21:01:21 -0300

    Collisions and player movement

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,13 +2,9 @@ extends Node2D
 var test_array: Array[String] = ["Test", "Blah"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Logo.rotation_degrees = 90
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Logo.rotation_degrees += 150 * delta
-	if $Logo.rotation_degrees > 180:
-		$Logo.rotation_degrees = 0
-
+	pass
 func test_function():
 	print("This is a test function")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends CharacterBody2D
 
-const speed : int = 300
+const speed : int = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +8,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var direction = Input.get_vector("left","right","up", "down")
-	position += direction*speed*delta
+	
+	#Usa delta automaticamente
+	velocity = direction * speed
+	move_and_slide()
 	
 	if Input.is_action_pressed("primary action"):
 		print("Shooting")
